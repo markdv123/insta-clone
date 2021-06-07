32a4605c15d66a0c0d0ee46deb83eccdd4a23191
Mark DeVito <markdv@alumni.unc.edu>
2021-06-07 15:32:33 -0400

    update user page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,16 @@
+import React, { useState, useEffect } from 'react'
+import { Switch, Route, withRouter } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './pages/Home'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import CreatePost from './pages/CreatePost'
+import UpdateUser from './pages/UpdateUser'
+import UpdatePost from './pages/UpdatePost'
+import ViewPost from './pages/ViewPost'
+import ViewUser from './pages/ViewUser'
 import './styles/App.css'
 
 function App() {
